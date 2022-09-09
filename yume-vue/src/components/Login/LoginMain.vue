332f14af4a526507578ddbee85cb50496b13b958
wzc <3283480860@qq.com>
2022-09-09 17:35:50 +0800

    2022-9-9

--- yume-vue/src/components/Login/LoginMain.vue
+++ yume-vue/src/components/Login/LoginMain.vue
@@ -1,7 +1,29 @@
 <template>
     <el-card class="box-card">
-        <el-form>
-            
+
+        <h3 align="center">用户登录</h3>
+        <!-- 向下顶开 -->
+        <div style="margin-top: 60px;" />
+        <!-- 表单 -->
+        <el-form style="margin-left: 140px;">
+            <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign"
+                style="max-width: 350px">
+                <el-form-item label="Name">
+                    <el-input v-model="formLabelAlign.name" />
+                </el-form-item>
+                <el-form-item label="Password">
+                    <el-input v-model="formLabelAlign.password" />
+                </el-form-item>
+
+                <el-form-item label="Verification">
+                    <el-input v-model="formLabelAlign.code" />
+                </el-form-item>
+                <!-- 提交框 -->
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">提交</el-button>
+                    <el-button type="register" @click="onRegister" >注册</el-button>
+                </el-form-item>
+            </el-form>
         </el-form>
     </el-card>
 </template>
@@ -11,9 +33,30 @@
 </script>
 
 <style scoped>
-.box-card{
+.box-card {
     margin: 0 auto;
-    width: 750px;
+    width: 650px;
     height: 350px;
+    background-color: rgba(255, 255, 255, 0.8);
 }
 </style>
+
+<script lang="ts" setup>
+    import { reactive, ref } from 'vue'
+    
+    const labelPosition = ref('left')
+    
+    const formLabelAlign = reactive({
+      name: '',
+      passwoed: '',
+      Code:'',
+    })
+
+    const onSubmit = () => {
+        alert('submit! 提交')
+    }
+
+    const onRegister = () => {
+        alert('register!')
+    }
+</script>
