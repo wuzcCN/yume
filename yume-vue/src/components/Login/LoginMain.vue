c80ccd718ef35b5967c53a9cbbfa54a99e777a83
wzc <3283480860@qq.com>
2022-09-09 18:59:03 +0800

    1.2

--- yume-vue/src/components/Login/LoginMain.vue
+++ yume-vue/src/components/Login/LoginMain.vue
@@ -1,7 +1,7 @@
 <template>
     <el-card class="box-card">
 
-        <h3 style="margin:0 260px;">用户登录</h3>
+        <h2 style="margin:0 250px;">用户登录</h2>
         <!-- 表单 -->
         <el-form style="margin-left: 140px;margin-top: 60px;">
             <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign"
@@ -27,26 +27,30 @@
 </template>
 
 <script setup>
-
-</script>
-
-<style scoped>
-.box-card {
-    margin: 0 auto;
-    width: 650px;
-    height: 350px;
-    background-color: rgba(255, 255, 255, 0.8);
-}
-</style>
-
-<script lang="ts" setup>
-    import { reactive, ref } from 'vue'
+    import {reactive, ref} from 'vue'
     
     const labelPosition = ref('left')
     
     const formLabelAlign = reactive({
-      name: '',
-      passwoed: '',
-      Code:'',
+      username: '',
+      password: '',
+      code: '',
     })
-</script>
+    
+    const onSubmit = () => {
+      alert("登录")
+    }
+    
+    const onRegister = () => {
+      alert("注册")
+    }
+    </script>
+    
+    <style scoped>
+    .box-card {
+      margin: 0 auto;
+      width: 650px;
+      height: 350px;
+      background-color: rgba(255, 255, 255, 0.8);
+    }
+    </style>
